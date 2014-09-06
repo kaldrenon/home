6d4d79ac9a01cea1f2179e7a420cac4926d0cab4
Andrew Fallows <kaldrenon@gmail.com>
2014-09-05 22:34:27 -0400

    delete unneeded files

--- winstall.bat
+++
@@ -1,8 +0,0 @@
-del %HOMEDRIVE%%HOMEPATH%\_vimrc
-del %HOMEDRIVE%%HOMEPATH%\_gvimrc
-rmdir /S /Q %HOMEDRIVE%%HOMEPATH%\vimfiles
-
-xcopy /S /E /C .vim\*.* %HOMEDRIVE%%HOMEPATH%\vimfiles\*.*
-copy .vimrc %HOMEDRIVE%%HOMEPATH%\_vimrc
-copy .gvimrc %HOMEDRIVE%%HOMEPATH%\_gvimrc
-
