7607240037f839c8d45d07ff269c97a0fcdceea6
Andrew Fallows <kaldrenon@gmail.com>
2015-06-30 14:15:53 -0400

    generalize symlinks

--- symlink.sh
+++ symlink.sh
@@ -2,5 +2,5 @@
 declare -a files=( .vimrc .tmux.conf .gitconfig .githelpers .zshrc antigen.zsh .vim )
 for file in ${files[@]}
 do
-  ln -s /root/home/$file /root/$file
+  ln -s ${HOME}/home/$file ${HOME}/$file
 done
